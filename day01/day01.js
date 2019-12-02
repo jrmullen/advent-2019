7af646b56128cd81f4ea44155d5b3bd30551d99d
Jeremy Mullen <jeremymullen@stockx.com>
2019-12-02 10:03:50 -0500

    Solve part 1

--- day01/day01.js
+++ day01/day01.js
@@ -2,5 +2,9 @@ const fs = require('fs');
 
 const input = fs.readFileSync('input.txt').toString().split('\n');
 
+let result = input.map(val => {
+    return Math.floor(val / 3) - 2;
+}).reduce((a, b) => a + b);
 
+console.log(`Part 1: ${result}`);
 
