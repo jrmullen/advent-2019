42c369c8fab95557147f6481d44b8ceca8fd7f09
Jeremy Mullen <jeremymullen@stockx.com>
2019-12-05 09:25:02 -0500

    Solve part 1

--- day04/day04.js
+++ day04/day04.js
@@ -2,5 +2,37 @@ const fs = require('fs');
 
 const input = fs.readFileSync('input.txt').toString().split('\n');
 
+const MIN = input[0].split('-')[0];
+const MAX = input[0].split('-')[1];
+
+let part1 = 0;
+
+let range = Array.from({ length: MAX - MIN }, (_, i) => (Number(MIN) + i).toString());
+
+range.forEach(element => {
+
+    let containsAdjacent = false;
+    let decreases = false;
+
+    for (let i = 0; i < element.length; i++) {
+        let current = Number(element[i]);
+        let next = Number(element[i +  1]);
+
+        if (current > next) {
+            decreases = true;
+        }
+
+        if (current === next) {
+            containsAdjacent = true;
+        }
+    }
+
+    if (containsAdjacent && !decreases) {
+        part1++;
+    }
+});
+
+console.log(`Part 1: ${part1}`);
+
 
 
